7b63ce9f3da8d1791e4bf577cd22056d7fc7c353
pennd44 <61793857+pennd44@users.noreply.github.com>
2022-08-03 22:59:06 -0500

    Replaced unit models and added running and idle animation in battle mode

---
+++ Tactics/Assets/Scripts/Components/Tile.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Color originalColor;
+    public Point pos;
+    public float height;
+    List<Tile> neighbors = new List<Tile>();
+    public int distance = int.MaxValue;
+    public Tile prev;
+    public GameObject content;
+    public bool occupied = false;
+    public bool selected = false;
+    public bool selectable = false;
+    public bool visited = false;
+    public int cost = 1;
+
+    private void findNeighbors(){
+
+    }
+    private void Awake() {
+        pos = new Point(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.z));
+        height = transform.position.y;
+        originalColor = gameObject.GetComponent<Renderer>().material.GetColor("_Color");
+    }
+}
---
+++ Tactics/Assets/Scripts/State Machine Related/States/ActionSelectState.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
+
+
+
+public class ActionSelectState : BattleState
+{
+
+    protected List<Tile> actionables;
+    public ActionSelectState(BattleStateMachine stateMachine) : base(stateMachine){}
+    public override void enter() {
+        ActionRange attack = unit.GetComponent<ActionRange>();
+        actionables = attack.GetTilesInRange(board);
+        board.SelectTiles(actionables, Color.red);
+        // unit.currentTile.distance = 0;
+        // actionables = board.Search(unit.currentTile, unit.attackRange);
+        // board.SelectTiles(actionables, Color.red);
+    }
+    public override void Tick(){
+        handleInput();
+    }
+    public override void exit(){
+        board.DeSelectTiles(actionables);
+    }
+
+    GameObject prevSelectedTile;
+    Color prevColor = Color.white;
+
+    public override void handleInput() {
+        /// Highlight tile mouse is hovering over
+        RaycastHit hit;
+        Tile hitTile;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            GameObject hitObj = hit.transform.gameObject;
+            if (hitObj.tag == "Tile")
+            {
+                hitTile = hitObj.GetComponent<Tile>();
+                if (prevSelectedTile != null)
+                {
+                    prevSelectedTile.GetComponent<Tile>().selected = false;
+                    prevSelectedTile.GetComponent<Renderer>().material.color = prevColor; // green -> blue but want green -> white
+                }
+                prevSelectedTile = hitObj;
+                hitTile.selected = true;
+                prevColor = hitTile.GetComponent<Renderer>().material.color;
+                hitTile.GetComponent<Renderer>().material.color= Color.green;
+                handleClick(hitTile);
+            }
+        }
+    }
+ 
+
+    //Helper Functions
+    private void handleClick(Tile hitTile)
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (hitTile.occupied && hitTile.selectable && hitTile.content.GetComponent<Character>() != null)
+            {
+                Character target = hitTile.content.GetComponent<Character>();
+                target.currentHealth -= unit.attack;
+                unit.canAct = false;
+                stateMachine.setState(new BattleMenuState(stateMachine));
+                ui.updateBars();
+            }
+        }
+
+    }
+}
