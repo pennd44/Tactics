2667ec4cc48466a04234013b4e44c3ff202afa68
pennd44 <61793857+pennd44@users.noreply.github.com>
2023-04-11 15:40:00 -0500

    enemy animated along patrol path

--- Tactics/Assets/Scripts/NPC BEHAVIOR/ENEMY COMPONENTS/NPCMovement.cs
+++ Tactics/Assets/Scripts/NPC BEHAVIOR/ENEMY COMPONENTS/NPCMovement.cs
@@ -7,16 +7,30 @@ using UnityEngine.AI;
 public class NPCMovement : MonoBehaviour
 {
     private NavMeshAgent navMeshAgent;
+    private Character character;
     [SerializeField] PatrolPath patrolPath;
     [SerializeField] float waypointTolerance = 1;
+    [SerializeField] float dwellTime = 3;
+    [SerializeField] float runSpeed = 4;
+    float timeSinceArrivedAtWaypoint = Mathf.Infinity;
     private Vector3 guardPosition;
+    float velocity;
     int currentWaypointIndex = 0;
     private void Start() {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        character = GetComponent<Character>();
     }
     private void Update() {
+        if(character.isDead){
+            enabled = false;
+            navMeshAgent.enabled = false;
+            return;
+            //move this to character.Die() so it's not checking every frame
+        }
+        timeSinceArrivedAtWaypoint += Time.deltaTime;
         PatrolBehavior();
-        
+        velocity = navMeshAgent.velocity.magnitude/runSpeed;
+        character.unitAnimator.SetFloat("Speed", velocity);
     }
     private void PatrolBehavior()
     {
@@ -24,11 +38,15 @@ public class NPCMovement : MonoBehaviour
         if(patrolPath != null){
             if(AtWaypoint())
             {
+                timeSinceArrivedAtWaypoint = 0;
                 CycleWaypoint();
             }
             nextPosition = GetCurrentWaypoint();
         }
-        navMeshAgent.destination = nextPosition;
+        if(timeSinceArrivedAtWaypoint> dwellTime){
+            navMeshAgent.destination = nextPosition;
+
+        }
     }
 
     private bool AtWaypoint()
